226d37ef02e556dd19e2ff1499d15d34753276d8
Eli <eliduper8@gmail.com>
2019-01-20 23:38:47 -0500

    Finalized connection to Firebase, errors removed

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:datetime_picker_formfield/time_picker_formfield.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
-import 'services/anouncementcode.dart';
 
 const appName = 'WTCS Anouncements';
 
@@ -25,21 +24,18 @@ class MyHomePage extends StatefulWidget {
 
 class MyHomePageState extends State<MyHomePage> {
   final dateFormat = DateFormat("MMMM d, yyyy");
-  DateTime date;
-  bool reviewFlag = false;
-  var reviews;
+  DateTime date ;
+  
+  // getAnouncement(DateTime date){
+  //   Firestore.instance
+  //       .collection('days')
+  //       .where('date', isEqualTo: date.toString())
+  //       .getDocuments().then((QuerySnapshot docs){
+  //         if(DocumentSnapshot.hasData){
 
-  /*@override
-  void initState(){
-    super.initState();
-    FetchAnouncements().getAnouncement(date).then((QuerySnapshot docs){
-if (docs.documents.isNotEmpty){
-  reviewFlag = true;
-  reviews = docs.documents[0].data;
-}
-
-    });
-  }*/
+  //         }
+  //       })
+  // }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -58,47 +54,19 @@ if (docs.documents.isNotEmpty){
               dateOnly: (true),
               maxLengthEnforced: true,
             ),
-            SizedBox(height: 16.0),
-            Text('$date', style: TextStyle(fontSize: 18.0)),
             Text(' \nAnnouncements \n', style: TextStyle(fontSize: 30.0)),
-            StreamBuilder(
-                stream: Firestore.instance.collection('asdf').snapshots(),
+             StreamBuilder(
+                stream: Firestore.instance.collection('days').snapshots(),
                 builder: (BuildContext context,
                     AsyncSnapshot<QuerySnapshot> snapshot) {
                   if (!snapshot.hasData) return CircularProgressIndicator();
-                  return FirestoreListView(documents: snapshot.data.documents);
-                })
+                  if(snapshot.data.documents[0][date.toString()] == null){
+                    return Text("No Anouncements Today");
+                  }
+                  return Text(snapshot.data.documents[0][date.toString()]??'');
+
+})
           ])));
 }
 
-class FirestoreListView extends StatelessWidget {
-  final List<DocumentSnapshot> documents;
 
-  FirestoreListView({this.documents});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: documents.length,
-      itemExtent: 90.0,
-      itemBuilder: (BuildContext context, int index) {
-        String title = documents[index].data['title'].toString();
-
-        return ListTile(
-            title: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5.0),
-                  border: Border.all(color: Colors.black),
-                ),
-                padding: EdgeInsets.all(5.0),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(title),
-                    )
-                  ],
-                )));
-      },
-    );
-  }
-}

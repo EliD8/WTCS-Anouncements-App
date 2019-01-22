75355968f8b6c8fcb7551f5441afa18b1a09c97e
Eli <eliduper8@gmail.com>
2019-01-21 23:04:08 -0500

    Added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,75 @@
 import 'dart:core';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 const appName = 'WTCS Anouncements';
 
-void main() => runApp(MaterialApp(
-      title: appName,
-      home: MyHomePage(),
-      theme: ThemeData.light().copyWith(
-          inputDecorationTheme:
-              InputDecorationTheme(border: OutlineInputBorder())),
+void main() {     
+      runApp(new MaterialApp(
+      home: Bigboyscreen(),
+      routes: <String, WidgetBuilder>{
+      "/SecondPage": (BuildContext context) => new MyHomePage(),
+      }
     ));
+}
 
+class Bigboyscreen extends StatefulWidget {
+  @override
+
+  _BigboyscreenState createState() => _BigboyscreenState();
+}
+class _BigboyscreenState extends State<Bigboyscreen> {
+
+  final background = Container(
+    decoration: BoxDecoration(
+    image: DecorationImage(
+      image: AssetImage('assets/background.jpg'),
+        fit: BoxFit.cover,
+    ),
+  ),
+);
+
+final whiteOpacity = Container(
+color:Color(0xAA69),
+);
+  
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Stack(
+        fit:StackFit.expand, 
+        children: <Widget>[
+          background, 
+          Column (
+            children: <Widget>[
+            Expanded (flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new IconButton(
+                  icon: new Icon(Icons.play_arrow, color: Colors.redAccent),
+                iconSize: 70.0,
+                onPressed: () {Navigator.of(context).pushNamed("/SecondPage");}
+                ),
+              ]
+            ),
+            
+            
+            ),
+            ],
+          ),
+        ],
+       ),
+    );
+    
+  }
+}
 class MyHomePage extends StatefulWidget {
   @override
   MyHomePageState createState() => MyHomePageState();
 }
 
 class MyHomePageState extends State<MyHomePage> {
-  final dateFormat = DateFormat("MMMM d, yyyy");
-  DateTime date;
+
 
   @override
   Widget build(BuildContext context) => Scaffold(
